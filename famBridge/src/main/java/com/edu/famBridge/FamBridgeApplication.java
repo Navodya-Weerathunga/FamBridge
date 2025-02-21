6f5dd5383cf56ede1b15d5f046f1f7664b25e65f
Dewindi-Weerathunga <164044212+Dewindi-Weerathunga@users.noreply.github.com>
2025-02-21 17:03:52 +0530

    first Backend commit

---
+++ famBridge/src/main/java/com/edu/famBridge/FamBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.edu.famBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamBridgeApplication.class, args);
+	}
+
+}
